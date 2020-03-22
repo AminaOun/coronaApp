1d3ec9e3987fb8ad3f1b60d16f886238380f3263
enghamzasalem <enghamzasalem@yahoo.com>
2020-03-22 14:49:19 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// Import the modules we need
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+
+// Define the URLS we will be scraping
+const baseURL = 'https://www.worldometers.info/coronavirus/';
+arr = [];
+// Define the method for collecting the data
+const getCountriesData = async () => {
+	const html = await rp(baseURL);
+	const tableHead = cheerio('#maincounter-wrap', html)
+		.map(function() {
+			arr.push(cheerio(this).html());
+			//console.log(cheerio(this).html());
+		})
+		.toArray();
+	console.log(arr);
+};
+getCountriesData();
+
+const express = require('express');
+const app = express();
+const port = 3055;
+
+app.get('/', (req, res) => {
+	getCountriesData();
+	res.send(arr);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
